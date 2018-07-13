d03edcb70e26ecd79b1b1f24ff018d85467f3208
mottledMantis <jamesmatheson35@gmail.com>
2018-07-13 11:59:12 -0400

    For submission

--- ititial R script.R
+++ ititial R script.R
@@ -28,3 +28,37 @@ HomeCredit_columns_description <- read_csv("data/HomeCredit_columns_description.
 # View(HomeCredit_columns_description)
 
 # to try: aggregate(table[cols to mean, eg 2:5]), list(table$categorical), mean)
+
+#combine application test and train into a single df called application_all, with TARGET as Column 2
+
+application_all <- merge(application_train, application_test, all = TRUE)
+application_all <- application_all[ , c(1, 122, 2:121)]
+
+#create df based on "bureau" grouped by SK_ID_CURR called bureau_grouped_ID
+
+bureau_grouped_ID <- group_by(bureau, SK_ID_CURR) %>% 
+  summarise("MAX_DAYS_LATE_BUREAU" = max(CREDIT_DAY_OVERDUE))
+
+# another way of doing the above: bureau_grouped_ID <- group_by(bureau, SK_ID_CURR)
+# bureau_grouped_ID <- summarise(bureau_grouped_ID, "max" = max(CREDIT_DAY_OVERDUE))
+
+#create a new "results" df that includes only our relevant info, one row per client
+
+results <- data.frame("SK_ID_CURR" = application_all$SK_ID_CURR,
+                      "TARGET" = application_all$TARGET,
+                      "ALL_INCOME" = application_all$AMT_INCOME_TOTAL)
+results <- merge(results, bureau_grouped_ID, by.x = 1, by.y = 1, all = TRUE)
+
+# Since NA values mean no information and we are looking for a positive flag
+#for overdue payments, might as well make NA values in "max" <- 0
+results$MAX_DAYS_LATE_BUREAU[is.na(results$MAX_DAYS_LATE_BUREAU)] <- 0
+
+#plot something
+# ggplot(results, aes(x = as.factor(results$TARGET), y = results$MAX_DAYS_LATE_BUREAU)) +
+#   geom_point()
+
+#results <- add_column(summarise(bureau_grouped_ID, max(CREDIT_DAY_OVERDUE)))
+
+# finalData<-subset(data,!(is.na(data["mmul"]) | is.na(data["rnor"]))) - use to remove rows with no TARGET value
+# use merge to combine the test and train sets?
+
